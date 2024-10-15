d823c6565e0a106e05c9772f19bccdb933c91ce2
DESKTOP-2J61KH8\vio <vivio823823@gmail.com>
2024-10-16 01:40:17 +0900

    no message

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   outputDir: '../docs', // リポジトリのルートに出力
   publicPath: process.env.NODE_ENV === 'production'
     ? '/aipriCompleateCheck/'
-    : '/'
+    : '/',
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "アイプリコンプチェック"
+    }
+  }
 }
